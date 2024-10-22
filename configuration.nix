70c72b4ea45b230413e0447498da7d558c2f2e1e
Nicolas Casatti <ncasatti@gmail.com>
2024-10-22 16:37:12 -0300

    Init commit nixos configuration

---
+++ configuration.nix
@@ -0,0 +1,235 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+#			./modules/nvidia.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+	boot.supportedFilesystems = ["ntfs"];
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Argentina/Cordoba";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_AR.UTF-8";
+    LC_IDENTIFICATION = "es_AR.UTF-8";
+    LC_MEASUREMENT = "es_AR.UTF-8";
+    LC_MONETARY = "es_AR.UTF-8";
+    LC_NAME = "es_AR.UTF-8";
+    LC_NUMERIC = "es_AR.UTF-8";
+    LC_PAPER = "es_AR.UTF-8";
+    LC_TELEPHONE = "es_AR.UTF-8";
+    LC_TIME = "es_AR.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.displayManager.defaultSession = "plasma";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "colemak";
+  };
+  console.useXkbConfig = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+	# Bluetooth
+	hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = false;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+	
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ncasatti = {
+    isNormalUser = true;
+    description = "ncasatti";
+    extraGroups = [ "networkmanager" "wheel" "docker" "adbusers" "kvm"];
+    packages = with pkgs; [
+      kdePackages.kate
+    #  thunderbird
+    ];
+  };
+
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "ncasatti";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+	services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true; # only needed for Wayland -- omit this when using with Xorg
+    openFirewall = true;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget 
+	environment.systemPackages = with pkgs; [
+	  #  wget
+
+		# Nix
+		home-manager
+
+		# Internet
+		brave
+		google-chrome
+		thunderbird
+		tidal-hifi
+		whatsapp-for-linux
+		remmina
+
+		# Utils
+		eza
+		zsh
+		fzf
+		piper
+		zoxide
+		ranger
+		freerdp3
+		magic-wormhole
+		libsForQt5.kdeconnect-kde
+
+		geogebra
+
+		## Development
+    # IDE
+		neovim
+		vscode
+    beekeeper-studio
+		android-studio
+		android-studio-tools
+    #zed-editor
+
+    # Utils
+    docker-compose
+		
+		# Node
+    nodejs_22
+		yarn
+		bun
+
+		# Video
+		obs-studio
+		
+    # Themes
+    libsForQt5.qtstyleplugin-kvantum
+		latte-dock
+  ];
+
+	# Programs
+	programs = {
+		firefox.enable = true;
+		git.enable = true;
+		adb.enable = true;
+	};
+
+	### Docker
+	virtualisation.docker.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+	services.ratbagd.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  networking.firewall = { 
+    enable = false;
+    allowedTCPPorts = [
+			5037 # ADB
+			5555 # ADB
+			47984 
+			47989 
+			47990 
+			48010
+		];
+		allowedUDPPorts = [
+			5037
+			5555
+		];
+    allowedTCPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];  
+    allowedUDPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+			{ from = 47998; to = 48000; }
+    	{ from = 8000; to = 8010; }
+    ];  
+  };  	
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
